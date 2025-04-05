76a0803f52db43b99f3758a1291eeba9ecd7c371
dknp1 <sureshnagpal992@gmail.com>
2025-04-05 11:22:17 +0530

    Add files via upload

---
+++ Maths operation.py
@@ -0,0 +1,22 @@
+first_number = int(input('Enter the first number: '))
+second_number = int(input('Enter the second number: '))
+
+def func(a, b):
+    print('Addition:', a + b)
+
+func(first_number, second_number)
+
+def func2(c, d):
+    print('Subtraction:', c - d)
+
+func2(first_number, second_number)
+
+def func3(d, e):
+    print('Multiplication:', d * e)
+
+func3(first_number, second_number)
+
+def func4(f, g):
+    print('Division:', f / g)
+
+func4(first_number, second_number)
