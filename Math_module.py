c111991489baa028cf371b8622d98cb7292343b0
dknp1 <sureshnagpal992@gmail.com>
2025-04-05 14:27:38 +0530

    Add files via upload

---
+++ Math_module.py
@@ -0,0 +1,11 @@
+import math
+
+user = float(input('Enter a number:'))
+
+square_root = math.sqrt(user)
+logarithm = math.log(user)
+sine = math.sin(user)
+
+print('square_root:', square_root)
+print('logarithm:', logarithm)
+print('sine:', sine)
